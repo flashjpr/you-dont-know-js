e975e98b1f872f88e4f42af719c7f13343851109
flashjpr <flashjpr@gmail.com>
2018-10-19 00:28:20 +0100

    Go away! Meaning of life generated by pausing and resuming the action

--- async patterns/async/exercises/generator.js
+++ async patterns/async/exercises/generator.js
@@ -1,41 +1,33 @@
-function* gen() {
-    console.log("Hello");
-    yield;
-    console.log("World!")
-}
-
-var it = gen();
-it.next();
-it.next();
-
-
-function* main() {
-    yield 1;
-    yield 2;
-    yield 3;
-}
-var itMain = main();
-
-it.next(); // {value: 1, done: false}
-it.next(); // {value: 2, done: false}
-it.next(); // {value: 3, done: false}
-
-it.next(); // {value: undefined, done: true}
-
-
-function coroutine(g) {
-    var it = g();
+function coroutine(generator) {
+    var it = generator();
     return function () {
         return it.next.apply(it, arguments);
     }
 }
 
+// var run = coroutine(function* () {
+//     var x = 1 + (yield);
+//     var y = 1 + (yield);
+//     yield (x + y);
+// });
+
+// run();
+// run(10);
+// console.log("Meaning of life: " + run(30).value);
+
+function getData(data) {
+    setTimeout(function () {
+        run(data);
+    }, 1000);
+}
+
 var run = coroutine(function* () {
-    var x = 1 + (yield);
-    var y = 1 + (yield);
-    yield (x + y);
+    var x = 1 + (yield getData(10));
+    var y = 1 + (yield getData(30));
+    var answer = (yield getData(
+        "Meaning of life: " + (x + y)
+    ));
+    console.log(answer);
 });
 
 run();
-run(10);
-console.log("Meaning of life: " + run(30).value);
