5d1d312303beaf059932dbe97415869d6e27872a
flashjpr <flashjpr@gmail.com>
2018-10-21 00:25:23 +0100

    cleanup

--- observables/observables.js
+++
@@ -1,8 +0,0 @@
-// rxjs : rx - reactive extensions
-
-// an observable is kind of like a chain of
-// calculated fields in a spreadsheet
-
-// an observable is an adaptor hooked on to an event source
-// that produces a promise every time a new event come through
-
